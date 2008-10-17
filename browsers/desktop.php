9a5d1ff2f15288d54ab3ec47e23e364315f77669
David Carrington <david.carrington@gmail.com>
2008-10-17 13:05:58 +0000

    New confirmation prompt when adding short tweets.

--- browsers/desktop.php
+++ browsers/desktop.php
@@ -6,7 +6,7 @@ function desktop_theme_external_link($url) {
 
 function desktop_theme_status_form($text = '') {
   if (user_is_authenticated()) {
-    return '<form method="POST" action="update">
+    return '<form method="POST" action="update" onsubmit="return confirmShortTweet();">
   <input id="status" name="status" value="'.$text.'" size="45" />
   <input type="submit" value="Update" /> <span id="remaining">140</span>
 </form>
@@ -14,6 +14,11 @@ function desktop_theme_status_form($text = '') {
 function updateCount() {
   document.getElementById("remaining").innerHTML = 140 - document.getElementById("status").value.length;
   setTimeout(updateCount, 200);
+}
+function confirmShortTweet() {
+  var len = document.getElementById("status").value.length;
+  if (len < 30) return confirm("That\'s a short tweet.\nContinue?");
+  return true;
 }
 updateCount();
 </script>';
