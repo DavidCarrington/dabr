808e7af43c35a4545ca3ec2a875ca810c9e782a7
David Carrington <david.carrington@gmail.com>
2008-11-06 13:15:18 +0000

    Multi-line text box for writing tweets from desktops (also G1/Android)

--- browsers/desktop.php
+++ browsers/desktop.php
@@ -1,14 +1,12 @@
-<?php
-
+<?php
 function desktop_theme_external_link($url) {
   return "<a href='$url'>$url</a>";
-}
-
+}
 function desktop_theme_status_form($text = '') {
   if (user_is_authenticated()) {
     return '<form method="POST" action="update" onsubmit="return confirmShortTweet();">
-  <input id="status" name="status" value="'.$text.'" size="45" />
-  <input type="submit" value="Update" /> <span id="remaining">140</span>
+  <textarea id="status" name="status" rows="3" style="width:100%; max-width: 400px;">'.$text.'</textarea>
+  <div><input type="submit" value="Update" /> <span id="remaining">140</span></div>
 </form>
 <script type="text/javascript">
 function updateCount() {
@@ -23,12 +21,10 @@ function confirmShortTweet() {
 updateCount();
 </script>';
   }
-}
-
+}
 
 function desktop_theme_search_form($query) {
   $query = stripslashes(htmlentities($query));
   return "<form action='search' method='GET'><input name='query' value=\"$query\" size='45' /><input type='submit' value='Search' /></form>";
-}
-
+}
 ?>
