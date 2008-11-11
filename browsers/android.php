8bdb2fc074de019282ab34425e60ca2400116f32
David Carrington <david.carrington@gmail.com>
2008-11-11 10:17:07 +0000

    Google Android will now show full-size avatars

---
+++ browsers/android.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'desktop.php';
+function anroid_theme_external_link($url) {
+  return "<a href='$url'>$url</a>";
+}
+function android_theme_status_form($text = '') {
+  return desktop_theme_status_form($text);
+}
+function android_theme_search_form($query) {
+  return desktop_theme_search_form($query);
+}
+
+function android_theme_avatar($url, $force_large = false) {
+  return "<img src='$url' />";
+}
+?>
