a2fde67d00aea13cc1191abdb4adda22f435a5db
David Carrington <david.carrington@gmail.com>
2009-01-20 08:42:24 +0000

    Fixed the BASE_URL logic.

--- config.sample.php
+++ config.sample.php
@@ -8,10 +8,9 @@ define('FLICKR_API_KEY', '');
 
 // Base URL, should point to your website, including a trailing slash
 // Can be set manually but the following code tries to work it out automatically.
-$base_url = 'http://'.$_SERVER['HTTP_HOST'];
 if ($directory = trim(dirname($_SERVER['SCRIPT_NAME']), '/\,')) {
-  $base_url .= "/$directory/";
+  $base_url .= '/'.$directory;
 }
-define('BASE_URL', $base_url);
+define('BASE_URL', $base_url.'/');
 
 ?>
